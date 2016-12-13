6e99835c38854198e2dcae299cac1a664c176b1a
yigao <445267987@qq.com>
2016-12-13 18:37:38 +0800

    add file

---
+++ scut_mmo/ScutMMO/ScutMMO/Script/CsScript/Common/SystemGlobal.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using GameServer.Script.Logic;
+using GameServer.Script.Model;
+using ZyGames.Framework.Common.Log;
+
+namespace GameServer.Script.Common
+{
+    public static class SystemGlobal
+    {
+        private static readonly object thisLock = new object();
+        private static bool m_IsRunning;
+
+        public static bool IsRunning
+        {
+            get { return m_IsRunning; }
+        }
+
+        public static void CloseRunState()
+        {
+            lock(thisLock)
+            {
+                m_IsRunning = false;
+            }
+        }
+
+        static SystemGlobal()
+        {
+            m_IsRunning = false;
+        }
+
+        public static void  Start()
+        {
+            lock (thisLock)
+            {
+                m_IsRunning = false;
+            }
+            Stopwatch allWatch = new Stopwatch();
+            allWatch.Start();
+
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                //加载配置
+                stopwatch.Start();
+                ConfigMgr.Inst().InitConfigMgr();
+                stopwatch.Stop();
+
+                TraceLog.ReleaseWrite("加载游戏execl配置文件完成，共花费时间 {0} ms", stopwatch.Elapsed.TotalMilliseconds);
+            }
+
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                MapMgr.Inst().Init();
+                stopwatch.Stop();
+                TraceLog.ReleaseWrite("加载游戏地图数据完成，共花费时间 {0} ms", stopwatch.Elapsed.TotalMilliseconds);
+            }
+
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                SceneMgr.Inst().Init();
+                stopwatch.Stop();
+                TraceLog.ReleaseWrite("创建场景完成，共花费时间 {0} ms", stopwatch.Elapsed.TotalMilliseconds);
+            }
+
+
+            allWatch.Stop();
+            TraceLog.ReleaseWrite("加载全部配置，共花费时间 {0} ms", allWatch.Elapsed.TotalMilliseconds);
+            
+            lock (thisLock)
+            {
+                m_IsRunning = true;
+            }
+        }
+
+        public static void Stop()
+        {
+
+        }
+    }
+}
---
+++ scut_mmo/ScutMMO/ScutMMO/Script/CsScript/Logic/Scene/OtherCreatureManaget.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Script.Logic
+{
+    public class OtherCreatureManaget
+    {
+        private readonly Dictionary<uint, Creature> m_Creatures = new Dictionary<uint, Creature>();
+        public Dictionary<uint, Creature> Creatures
+        {
+            get { return m_Creatures; }
+        }
+
+        Creature GetCreature(uint cid)
+        {
+            Creature creature = null;
+            Creatures.TryGetValue(cid, out creature);
+            return creature;
+        }
+
+        public void AddCreature(Creature creature)
+        {
+            if (creature != null)
+            {
+                Creatures.Add(creature.Cid, creature);
+            }
+        }
+
+        public bool RemoveCreature(uint cid)
+        {
+            return Creatures.Remove(cid);
+        }
+    }
+}
